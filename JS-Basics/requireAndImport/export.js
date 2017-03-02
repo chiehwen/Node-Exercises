1fca9204c2ab2575832ed3ef91df6838b3c034ce
Chieh-Wen Yang <chuck.yang@hackathon.tw>
2017-03-02 22:39:42 +0800

    Add requireAndImport

---
+++ JS-Basics/requireAndImport/export.js
@@ -0,0 +1,24 @@
+let myVariable = 888;
+export let myVar1 = 777;
+module.exports.myVar2 = 999;
+
+let func1 = () => {
+    console.log("Hello from func1.");
+}
+
+let func2 = () => {
+    console.log("Hello from func2.");
+}
+
+// module.exports.func3 = () => {
+//     console.log("Hello from func3.");
+// }
+
+// module.exports.myVar1 = myVariable;
+// module.exports.func1 = func1;
+// module.exports.func2 = func2;
+
+// module.exports = {
+//     func1: func1,
+//     func2: func2
+// };
---
+++ JS-Basics/requireAndImport/requireAndImport.js
@@ -0,0 +1,17 @@
+// let myFuncs = require("./export");
+import { myVar1, myVar2, func1, func2, func3 } from "./export";
+
+
+// console.log(`The value of myVar1 is ${myFuncs.myVar1}`);
+// console.log(`The value of myVar2 is ${myFuncs.myVar2}`);
+
+// myFuncs.func1;
+// myFuncs.func2;
+// myFuncs.func3;
+
+console.log(`The value of myVar1 is ${myVar1}`);
+console.log(`The value of myVar2 is ${myVar2}`);
+
+// func1();
+// func2();
+// func3();
