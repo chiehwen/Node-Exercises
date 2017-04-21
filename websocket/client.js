a74cfaf4b91f748109e18409185de463c24dfd3e
Chieh-Wen Yang <chuck.yang@hackathon.tw>
2017-04-21 09:19:27 +0800

    Add comment to WebSocket.

--- websocket/client.js
+++ websocket/client.js
@@ -4,6 +4,7 @@ const ws = new WebSocket('ws://localhost:8080');
 
 // Send message to WebSocket server
 ws.on('open', () => {
+    console.log('WebSocket connection has been established.')
     ws.send('The message from client.');
 });
 
